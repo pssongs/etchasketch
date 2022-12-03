d336f8261a24349551143107fa4bbdbe1a7b602f
Paul Song <jaebinpsong@gmail.com>
2022-12-03 00:37:06 -0800

    added a slider to control number of squares

--- board.js
+++ board.js
@@ -1,5 +1,9 @@
 function generateDivs(e){
-    num = e.path[0].id
+    if(isNaN(e)){
+        num=e.path[0].value
+    } else {
+        num = e
+    }
     var container = document.querySelector('#container')
     container.innerHTML = ''
     for (let i = 0; i < num; i++){
@@ -8,10 +12,15 @@ function generateDivs(e){
         for (let x = 1; x <= num; x++){
             var cell = document.createElement('div')
             cell.className = 'gridsquare'
+            let length = (600/num)
+            cell.style.height = `${length}px`
+            cell.style.width = `${length}px`
             row.appendChild(cell)
         }
         container.appendChild(row)
     }
+    let value = document.querySelector('#rangeNumber')
+    value.textContent = `${num}`
     fill()
 }
 function fill(){
@@ -22,8 +31,14 @@ function fill(){
         })
 })
 }
-const buttons = document.querySelectorAll('.button')
-buttons.forEach(button => {
-    button.addEventListener('click', generateDivs)
-},false)
+// const buttons = document.querySelectorAll('.button')
+// buttons.forEach(button => {
+//     button.addEventListener('click', generateDivs)
+// },false)
 
+
+const range = document.querySelector('#range')
+window.onload = generateDivs(25)
+range.addEventListener('input', generateDivs)
+
+console.log(range)
